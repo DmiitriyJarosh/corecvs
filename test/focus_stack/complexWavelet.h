a82a141b764cbc09b01f1d90a37db5d1328afcb5
olesyagogina <olesiag07@yandex.ru>
2020-01-04 12:59:03 +0300

    updated

--- test/focus_stack/complexWavelet.h
+++ test/focus_stack/complexWavelet.h
@@ -12,13 +12,13 @@ public:
     ComplexWavelet(){}
     void doStacking(vector<corecvs::RGB24Buffer*> & imageStack, corecvs::RGB24Buffer * result) override;
     ~ComplexWavelet() override {}
-    static void test() {}
+    static void test();
 
 private:
     static corecvs::AbstractBuffer<double> * merge(corecvs::AbstractBuffer<double> * in, int type1, int type2);
     static corecvs::AbstractBuffer<double> * split(corecvs::AbstractBuffer<double> * in, int type1, int type2);
 
-    static pair<corecvs::AbstractBuffer<double> *, corecvs::AbstractBuffer<double> *> analysis(corecvs::DpImage * in, int n, int length);
+    static pair<corecvs::AbstractBuffer<double> *, corecvs::AbstractBuffer<double> *> analysis(corecvs::DpImage * in);
     static void subtract(corecvs::AbstractBuffer<double> * im1, corecvs::AbstractBuffer<double> * im2);
     static void add(corecvs::AbstractBuffer<double> * im1, corecvs::AbstractBuffer<double> * im2);
     static corecvs::AbstractBuffer<double> * synthesis(corecvs::AbstractBuffer<double> * inRe, corecvs::AbstractBuffer<double> * inIm, int n);
